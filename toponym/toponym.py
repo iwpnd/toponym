288de046a8f8d550016b79bee5f5fbc41abb4c77
iwpnd <legionaerr@googlemail.com>
2019-10-24 21:11:02 +0200

    remoed f-string to still support p3.5 Ä

--- toponym/toponym.py
+++ toponym/toponym.py
@@ -85,8 +85,6 @@ class Toponym(case.Case):
                 dd[key].append(val)
 
         for k, v in dd.items():
-            # if isinstance(v[0], str):
-            #     dd[k] = " ".join([x for x in dd[k]])
 
             if all([isinstance(x, str) for x in v]):
                 dd[k] = " ".join([x for x in dd[k]])
