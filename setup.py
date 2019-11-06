d3bc1bb3414a8cb1f13cfc573cf479b4bed0d539
iwpnd <legionaerr@googlemail.com>
2019-11-06 20:58:19 +0100

    add packages to setup.py, added test for settings.PARENT_DIRECTORY and settings.TOPODICT_DIR and utils.get_language_code()

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
       license='MIT',
       include_package_data=True,
       install_requires=[],
+      packages=packages,
       zip_safe=False,
       classifiers=[
             'Programming Language :: Python :: 3',
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,8 +1,41 @@
-from toponym import utils
+from toponym import utils, settings
+import pytest
+import os
 
 def test_get_available_languages():
-    """get avalable languages as ISO 639-1
+    """get available languages as ISO 639-1
     """
 
     lg = utils.get_available_language_codes()
     assert lg
+
+
+def test_get_language_code_success():
+    language_code = utils.get_language_code('russian')
+    assert language_code
+    assert language_code == 'ru'
+
+
+def test_get_language_code_fails():
+    with pytest.raises(KeyError):
+        language_code = utils.get_language_code('kaudawelsh')
+
+
+
+def test_topodict_dir():
+    list_dir = os.listdir(settings.TOPODICT_DIR)
+
+    assert 'hr.json' in list_dir
+    assert 'ru.json' in list_dir
+
+
+def test_parent_directory():
+    list_dir = os.listdir(settings.PARENT_DIRECTORY)
+
+    assert all([file in list_dir for file in ['toponym.py',
+                'resources',
+                '__init__.py',
+                'utils.py',
+                'topodict.py',
+                'settings.py',
+                'case.py']])
