8f2abb1afa4d92072de6ee46e3f0325195852b2f
Ben <iwpnd@users.noreply.github.com>
2020-01-03 13:46:20 +0100

    Add polish topodict (#12)
    * added pl, added consistency test for topodictionaries, beautified old topodicts
    
    * bumped version
    
    * remove fstring for p3.5 compatibility

--- toponym/settings.py
+++ toponym/settings.py
@@ -1,7 +1,7 @@
 import os
 
 PARENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-TOPODICT_DIR = os.path.join(PARENT_DIRECTORY, 'resources')
+TOPODICT_DIR = os.path.join(PARENT_DIRECTORY, "resources")
 
 LANGUAGE_DICT = {
     "croatian": "hr",
@@ -10,5 +10,6 @@ LANGUAGE_DICT = {
     "romanian": "ro",
     "latvian": "lv",
     "hungarian": "hu",
-    "greek": "el"
+    "greek": "el",
+    "polish": "pl",
 }
