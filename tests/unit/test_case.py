0e9ffa20579f21266c0390f53d3aa1931fd67220
iwpnd <legionaerr@googlemail.com>
2020-02-07 11:48:23 +0100

    added CaseConfig class to handle config elements. started to separated build_case into build_case_from_string and build_cases_from_list

--- tests/unit/test_case.py
+++ tests/unit/test_case.py
@@ -1,3 +1,4 @@
+from toponym.case import build_case_from_string
 from toponym.case import Case
 
 default_recipe = {
@@ -8,11 +9,15 @@ default_recipe = {
 }
 
 
-def test_case_build_case_config():
-    config = {"input_word": "Test", "new_word_ending": "i", "cut_ending_by": 1}
-
+def test_case_build_case_config(case_config):
     test_case = Case()
-    test_case._build_case(config)
+    test_case._build_case(case_config)
+
+
+def test_case_build_from_string_success(case_config):
+    output_word = build_case_from_string(case_config)
+
+    assert output_word
 
 
 def test_case_constructor_config():
--- toponym/case.py
+++ toponym/case.py
@@ -1,44 +1,55 @@
 from typing import Union
 
 
+class CaseConfig:
+    """A configuration for the case
+
+    Attributes:
+    input_word: Input word/words
+    new_word_ending: word ending or multiple
+    cut_ending_by: amount of characters cut from input_word before new_word_ending is added
+    """
+
+    input_word: Union[list, str]
+    new_word_ending: Union[list, str]
+    cut_ending_by: int
+
+
 class Case(object):
     def __init__(self):
         pass
 
-    def _build_case(self, config: dict) -> Union[list, str]:
+    def _build_case(self, config: CaseConfig) -> Union[list, str]:
         """ Modify a word given an ending and a cutending parameter
         """
 
-        is_str = isinstance(config["input_word"], str)
-        is_list = isinstance(config["input_word"], list)
+        is_str = isinstance(config.input_word, str)
+        is_list = isinstance(config.input_word, list)
 
         if not (is_str or is_list):
             raise TypeError(
-                type(config["input_word"]),
+                type(config.input_word),
                 "- is not supported. Either provide str or list of str",
             )
 
         if is_str:
-            if config["cut_ending_by"] != 0:
+            if config.cut_ending_by != 0:
                 output_word = (
-                    config["input_word"][: -config["cut_ending_by"]]
-                    + config["new_word_ending"]
+                    config.input_word[: -config.cut_ending_by] + config.new_word_ending
                 )
             else:
-                output_word = config["input_word"] + config["new_word_ending"]
+                output_word = config.input_word + config.new_word_ending
             return output_word
 
         if is_list:
             output_words = list()
 
-            for ending in config["new_word_ending"]:
-                if config["cut_ending_by"] != 0:
-                    output_word = (
-                        config["input_word"][: -config["cut_ending_by"]] + ending
-                    )
+            for ending in config.new_word_ending:
+                if config.cut_ending_by != 0:
+                    output_word = config.input_word[: -config.cut_ending_by] + ending
                     output_words.append(output_word)
                 else:
-                    output_word = config["input_word"] + ending
+                    output_word = config.input_word + ending
                     output_words.append(output_word)
             return output_words
 
@@ -50,6 +61,11 @@ class Case(object):
             "new_word_ending": config["recipe"][0],
             "cut_ending_by": config["recipe"][1],
         }
+
+        case_config = CaseConfig()
+        case_config.input_word = config["input_word"]
+        case_config.new_word_ending = config["recipe"][0]
+        case_config.cut_ending_by = config["recipe"][1]
 
         if isinstance(config["input_word"], list) and len(config["recipe"][0]) == 1:
             output_words = []
@@ -68,3 +84,14 @@ class Case(object):
                     output_words.append(output_word)
 
                 return output_words
+
+
+def build_case_from_string(config: CaseConfig) -> str:
+    if config.cut_ending_by != 0:
+        output_word = (
+            config.input_word[: -config.cut_ending_by] + config.new_word_ending
+        )
+    else:
+        output_word = config.input_word + config.new_word_ending
+
+    return output_word
