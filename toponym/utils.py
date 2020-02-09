27c12d231fd35a4239ec4d54360e19d9912d24f1
iwpnd <legionaerr@googlemail.com>
2020-02-09 09:14:05 +0100

    renamed utility functions to reflect new naming convention

--- toponym/utils.py
+++ toponym/utils.py
@@ -7,14 +7,18 @@ from . import settings
 def get_available_language_codes() -> str:
     """Returns a list of available languages and their 2 char input codes
     """
-    topodict_files = os.listdir(os.path.join(settings.TOPODICT_DIR))
-    two_dig_codes = [f.split(".")[0] for f in topodict_files if f.endswith(".json")]
+    recipes_filespaths = os.listdir(os.path.join(settings.RECIPES_DIR))
+    ISO_639_1_code = [
+        filepath.split(".")[0]
+        for filepath in recipes_filespaths
+        if filepath.endswith(".json")
+    ]
 
-    for d in two_dig_codes:
-        if not d == "_test":
-            assert len(d) == 2
-    two_dig_codes.sort()
-    return two_dig_codes
+    for code in ISO_639_1_code:
+        if not code == "_test":
+            assert len(code) == 2
+    ISO_639_1_code.sort()
+    return ISO_639_1_code
 
 
 def get_language_code(language: str) -> str:
@@ -38,13 +42,15 @@ def print_available_languages() -> None:
     print()
 
 
-def load_topodict(language_code: str) -> dict:
+def load_recipes(language_code: str) -> dict:
     """
     Loads language-specific stopwords for keyword selection
     """
 
-    topodictFile = os.path.join(settings.TOPODICT_DIR, "{}.json".format(language_code))
+    recipes_file_path = os.path.join(
+        settings.RECIPES_DIR, "{}.json".format(language_code)
+    )
 
-    with open(topodictFile, "r", encoding="utf-8") as f:
-        tdict = json.loads(f.read())
-        return tdict
+    with open(recipes_file_path, "r", encoding="utf-8") as f:
+        recipes_file = json.loads(f.read())
+        return recipes_file
