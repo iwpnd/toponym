1df49543752127fa5e3673c07b0fa5c7f44ee843
iwpnd <legionaerr@googlemail.com>
2020-02-11 13:47:13 +0100

    some cleanup, some docstrings, some examples, separated merge_list_of_case_dictionaries from concat_case_dictionaries and moved them separately to build() for better readibility and to reduce the number of abstractions

--- toponym/toponym.py
+++ toponym/toponym.py
@@ -1,13 +1,12 @@
 import itertools
-import logging
 from collections import defaultdict
 from typing import Generator
+
+from loguru import logger
 
 from toponym.case import Case
 from toponym.case import DeclineConfig
 from toponym.recipes import Recipes
-
-logger = logging.getLogger(__name__)
 
 
 class Toponym:
@@ -24,11 +23,14 @@ class Toponym:
         decline_config = DeclineConfig()
 
         if self.input_is_multiple_words:
-            self.toponyms = concat_case_dictionaries(
+            self.merged_toponym_dictionaries = merge_list_of_case_dictionaries(
                 self._build_toponym_for_multiple_input_words(
                     decline_config=decline_config
                 )
             )
+            print(self.merged_toponym_dictionaries)
+
+            self.toponyms = concat_case_dictionaries(self.merged_toponym_dictionaries)
 
         else:
             self.toponyms = self._build_toponym_for_input_word(
@@ -36,6 +38,9 @@ class Toponym:
             )
 
     def _build_toponym_for_input_word(self, decline_config: DeclineConfig) -> dict:
+        """ builds a toponym from a single input_word
+        """
+
         recipe = get_recipe_for_input_word(
             input_word=self.input_word, recipes=self.recipes
         )
@@ -52,6 +57,11 @@ class Toponym:
     def _build_toponym_for_multiple_input_words(
         self, decline_config: DeclineConfig
     ) -> Generator:
+        """builds toponyms if self.input_is_multiple_words == True
+
+        Since there is more than one word, there should be a toponym for
+        every input word present.
+        """
 
         for _, input_word in enumerate(self.input_words):
             recipe = get_recipe_for_input_word(
@@ -81,23 +91,28 @@ class Toponym:
             raise Exception(".build() first")
 
 
-def concat_case_dictionaries(list_of_case_dictionaries: Generator) -> dict:
-    """ Concate list of dictionaries
-    """
-    merged_toponym_dictionaries = merge_list_of_case_dictionaries(
-        list_of_case_dictionaries
-    )
-
-    for case, toponym_word in merged_toponym_dictionaries.items():
-
-        product = itertools.product(*toponym_word)
-        permutation = [" ".join([y for y in x]) for x in product]
-        merged_toponym_dictionaries[case] = permutation
-
-    return merged_toponym_dictionaries
-
-
 def merge_list_of_case_dictionaries(list_of_case_dictionaries: Generator) -> dict:
+    """Take a generator of case dictionaries and hash them into a
+    defaultdict.
+
+    If the input_word is a combination of two words, the input_word is split()
+    and _build_toponym_for_multiple_input_words() is used to build the toponym.
+    In the process it is possible that one of the input words has more than one
+    possible new_word_ending, so the possible toponyms are appended as list.
+
+    returns
+
+    Example:
+
+    {
+        'nominative': [['Beograq'], ['Begraf']],
+        'genitive': [['Beograq'], ['Begrafa']],
+        'dative': [['Beograq'], ['Begrafu']],
+        'accusative': [['Beograq'], ['Begrafa']],
+        'locative': [['Beograq'], ['Begrafu']],
+        'instrumental': [['Beograq'], ['Begrafem', 'Begrafom']]
+        }
+    """
 
     merged_toponym_dictionaries = defaultdict(list)
 
@@ -108,13 +123,71 @@ def merge_list_of_case_dictionaries(list_of_case_dictionaries: Generator) -> dic
     return merged_toponym_dictionaries
 
 
+def concat_case_dictionaries(merged_toponym_dictionaries: defaultdict) -> dict:
+    """ Take a defaultdict and create products from the dicts values.
+
+    In case there are multiple input_words and one of them has multiple possible
+    new endings, we have to create products of those.
+
+    returns
+    Example:
+
+    {
+        'nominative': ['Beograq Begraf'],
+        'genitive': ['Beograq Begrafa'],
+        'dative': ['Beograq Begrafu'],
+        'accusative': ['Beograq Begrafa'],
+        'locative': ['Beograq Begrafu'],
+        'instrumental': ['Beograq Begrafem', 'Beograq Begrafom']
+        }
+    """
+
+    for case, toponym_word in merged_toponym_dictionaries.items():
+
+        product = itertools.product(*toponym_word)
+        permutation = [" ".join([y for y in x]) for x in product]
+        merged_toponym_dictionaries[case] = permutation
+
+    return merged_toponym_dictionaries
+
+
 def get_recipe_for_input_word(input_word: str, recipes: Recipes) -> dict:
+    """Get a recipe from a list of recipes based on the longest matching
+    character sequence found in the input_word that is also in Recipes.keys()
+
+    returns recipe
+
+    {
+        'nominative': [[''], 0],
+        'genitive': [['e'], 1],
+        'dative': [['i'], 1],
+        'accusative': [['u'], 1],
+        'locative': [['i'], 1],
+        'instrumental': [['om'], 1]
+        }
+    """
     recipe = recipes[get_longest_word_ending(input_word=input_word, recipes=recipes)]
     return recipe
 
 
 def get_longest_word_ending(input_word: str, recipes: Recipes) -> str:
-    """Disect word into differnet size shifs
+    """Disect word into differnet size shifs and return the longest shif that
+    is also within recipes.keys()
+
+    returns str
+
+    Example:
+
+    recipes = {
+        "_default": {"nominative": [[""], 0], "genitive": [[""], 0]},
+        "i": {"nominative": [[""], 0], "genitive": [["o"], 1]},
+        "o": {"nominative": [[""], 0], "genitive": [["a"], 1]},
+        "ti": {"nominative": [[""], 0], "genitive": [["o"], 1]},
+        "esti": {"nominative": [[""], 0], "genitive": [["o", "a"], 1]},
+    }
+
+    input_word = "Testi"
+    >> "esti"
     """
 
     matching_endings = [
